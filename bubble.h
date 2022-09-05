1c20dd7854e5b8c2d76aee6841fb3b0400a5bd23
Gamaliel-Marines <110830802+Gamaliel-Marines@users.noreply.github.com>
2022-09-05 12:44:50 -0500

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -20,40 +20,41 @@
 //
 // @param A, an array of T elements.
 // @param size, the number of elements in the array.
+// @return the number of swaps.
+// @complexity O(n^2)
 // =================================================================
 template <class T>
-void bubbleSort(T *arr, int size) {
+int bubbleSort(T *arr, int size) {
+	int counter{};
 	for(int i = size - 1; i > 0; i--){
 		for(int j = 0; j < i; j++){
+			++counter;
 			if(arr[j] > arr[j + 1]){
 				swap(arr, j, j + 1);
 			}
 		}
 	}
+	return counter;
 }
-
 // =================================================================
 // Performs the bubble sort algorithm on an vector.
 //
 // @param v, a vector of T elements.
+// @return the number of swaps.
+// @complexity O(n^2)
 // =================================================================
 template <class T>
-std::vector<int>bubbleSort(std::vector<T> &v)
-{
-	int compare = 0;
-	for(int i = v.size() - 1; i > 0; i--)
-	{
-		for(int j = 0; j < i; j++)
-		{
-			if(v[j] > v[j + 1])
-			{
+int bubbleSort(std::vector<T> &v) {
+	int counter{};
+	for(int i = v.size() - 1; i > 0; i--){
+		for(int j = 0; j < i; j++){
+			if(v[j] > v[j + 1]){
 				swap(v, j, j + 1);
-				compare++;
+				++counter;
 			}
 		}
 	}
-	v.push_back(compare);
-	return v;
+	return counter;
 }
 
 #endif /* BUBBLE_H */
